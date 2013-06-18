d8b25d58e353c0ea16c026960ac55866abda476b
Curtis Rueden <ctrueden@wisc.edu>
2013-06-18 15:44:34 -0500

    Apply ImageJ formatting template

--- src/main/java/imagej/server/Main.java
+++ src/main/java/imagej/server/Main.java
@@ -21,52 +21,55 @@
 
 package imagej.server;
 
+import java.io.IOException;
+import java.net.URI;
+
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import java.io.IOException;
-import java.net.URI;
-
 /**
  * Main class.
- *
  */
 public class Main {
-    // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/imagej/";
 
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     * @return Grizzly HTTP server.
-     */
-    public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in imagej.server package
-        final ResourceConfig rc = new ResourceConfig().packages("imagej.server");
+	/** Base URI the Grizzly HTTP server will listen on. */
+	public static final String BASE_URI = "http://localhost:8080/imagej/";
 
-        // uncomment the following line if you want to enable
-        // support for JSON on the service (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml)
-        // --
-        // rc.addBinder(org.glassfish.jersey.media.json.JsonJaxbBinder);
+	/**
+	 * Starts Grizzly HTTP server exposing JAX-RS resources defined in this
+	 * application.
+	 * 
+	 * @return Grizzly HTTP server.
+	 */
+	public static HttpServer startServer() {
+		// create a resource config that scans for JAX-RS resources and providers
+		// in imagej.server package
+		final ResourceConfig rc = new ResourceConfig().packages("imagej.server");
 
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
+		// uncomment the following line if you want to enable
+		// support for JSON on the service (you also have to uncomment
+		// dependency on jersey-media-json module in pom.xml)
+		// --
+		// rc.addBinder(org.glassfish.jersey.media.json.JsonJaxbBinder);
 
-    /**
-     * Main method.
-     * @param args
-     * @throws IOException
-     */
-    public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
-    }
+		// create and start a new instance of grizzly http server
+		// exposing the Jersey application at BASE_URI
+		return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+	}
+
+	/**
+	 * Main method.
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(final String[] args) throws IOException {
+		final HttpServer server = startServer();
+		System.out.println(String.format(
+			"Jersey app started with WADL available at "
+				+ "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+		System.in.read();
+		server.stop();
+	}
 }
-
