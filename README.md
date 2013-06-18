3ad7d96adabb90c754a4042aab1de9cfd38e2904
Curtis Rueden <ctrueden@wisc.edu>
2013-06-18 15:40:03 -0500

    Initial version, generated from Jersey archetype
    Thanks to the Jersey documentation for the headstart:
    https://jersey.java.net/nonav/documentation/2.0/getting-started.html#new-from-archetype

---
+++ README.md
@@ -0,0 +1,3 @@
+This project will house a RESTful web server for ImageJ.
+
+Implementation and documentation coming soon!
---
+++ src/test/java/imagej/server/MyResourceTest.java
@@ -0,0 +1,69 @@
+/*
+ * #%L
+ * ImageJ server for RESTful access to ImageJ.
+ * %%
+ * Copyright (C) 2013 Board of Regents of the University of
+ * Wisconsin-Madison.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package imagej.server;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MyResourceTest {
+
+    private HttpServer server;
+    private WebTarget target;
+
+    @Before
+    public void setUp() throws Exception {
+        // start the server
+        server = Main.startServer();
+        // create the client
+        Client c = ClientBuilder.newClient();
+
+        // uncomment the following line if you want to enable
+        // support for JSON in the client (you also have to uncomment
+        // dependency on jersey-media-json module in pom.xml and Main.startServer())
+        // --
+        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
+
+        target = c.target(Main.BASE_URI);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        server.stop();
+    }
+
+    /**
+     * Test to see that the message "Got it!" is sent in the response.
+     */
+    @Test
+    public void testGetIt() {
+        String responseMsg = target.path("myresource").request().get(String.class);
+        assertEquals("Got it!", responseMsg);
+    }
+}
